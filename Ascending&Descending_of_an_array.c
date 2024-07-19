b2bfce1ed5388a1675bb34064c819905ac29f8f0
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-07-19 14:30:25 +0530

    Ascending&Descending_of_an_array.c

--- Ascending&Descending_of_an_array.c
+++ Ascending&Descending_of_an_array.c
@@ -36,33 +36,6 @@ int main()
         }
     }
     }
-    else
-    {
-    for(i=0;i<mid;i++)
-    {
-     for(j=i+1;j<mid;j++)
-     {
-         if(a[i]>a[j])
-         {
-             temp=a[i];
-             a[i]=a[j];
-             a[j]=temp;
-         }
-     }
-    }
-    for(i=mid;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[i]<a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-    }
     for(i=0;i<n;i++)
     printf("%d ",a[i]);
     return 0;
